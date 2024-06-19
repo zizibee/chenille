e33b99c7c27b41431c430b77abb75d77eb42e9c2
Zia B <62911175+zizibee@users.noreply.github.com>
2024-06-19 15:00:52 -0700

    init

---
+++ Chenille/ChenilleApp.swift
@@ -0,0 +1,32 @@
+//
+//  ChenilleApp.swift
+//  Chenille
+//
+//  Created by zi zi on 6/17/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ChenilleApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
